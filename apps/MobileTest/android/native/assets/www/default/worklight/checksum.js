ce82a6018f53d3df82052b2b8744f2ee4fcb0b35
Tiago Mesquita <tiagomac@gmail.com>
2021-07-13 18:42:09 -0300

    remoção de códigos nativos

--- apps/MobileTest/android/native/assets/www/default/worklight/checksum.js
+++
@@ -1,3 +0,0 @@
-var WL_CHECKSUM = {"checksum":3606036661,"date":1448114431324,"machine":"NTB137607.corporate.group"};
-var WL_SKINLOADER_CHECKSUM = {"checksum":3606163283};
-/* Date: Sat Nov 21 11:00:31 GMT-03:00 2015 */
--- apps/MobileTest/windowsphone8/native/www/default/worklight/checksum.js
+++
@@ -1,2 +0,0 @@
-var WL_CHECKSUM = {"checksum":683993315,"date":1448114418468,"machine":"NTB137607.corporate.group"};
-/* Date: Sat Nov 21 11:00:18 GMT-03:00 2015 */
